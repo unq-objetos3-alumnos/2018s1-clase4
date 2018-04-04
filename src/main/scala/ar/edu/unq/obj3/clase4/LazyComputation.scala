b52902cfafc466e505987d158d2314230352c618
Alvarez Ariel <AlvarezAriel@users.noreply.github.com>
2018-04-04 10:34:53 -0300

    Update LazyComputation.scala

--- src/main/scala/ar/edu/unq/obj3/clase4/LazyComputation.scala
+++ src/main/scala/ar/edu/unq/obj3/clase4/LazyComputation.scala
@@ -4,18 +4,34 @@ import ar.edu.unq.obj3.clase4.ComplicatedStrings._
 
 
 object ComplicatedStrings {
-  def nicer(a:String, b:String): String = {
-    if (isNice(a)) a else b
+  type BeautifulString = ()=>String
+
+  def nicer(a:BeautifulString, b:BeautifulString): String = {
+    val resultA = a.apply()
+    if (isNice(resultA)) resultA else b.apply()
   }
 
   private def isNice(text:String): Boolean = text.length > 5
 
-  def beautify(text:String): String = {
-    println(s"ejecutando beautify: $text")
-    text
+  def beautify(text:String): BeautifulString = {
+    ()=> {
+      println(s"ejecutando beautify: $text")
+      text
+    }
   }
 }
 
+case class Perro(nombre:String, edad:Int)
+
+object Algo {
+  Perro("pepe", 5).copy(nombre = "boby")
+}
+
+
 object LazyComputation extends App {
-  val result = nicer(beautify("uno"), beautify("dos"))
+  val result = nicer(
+    beautify("unodostrescuatro"),
+    beautify("dos")
+  )
+  println(s"El más lindo es $result")
 }
